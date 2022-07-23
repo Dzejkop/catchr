13845b193b749d04863d53850ba7db699cebd764
Dzejkop <jakubtrad@gmail.com>
2022-07-23 14:39:16 +0200

    rustfmt

--- catchr/src/lib.rs
+++ catchr/src/lib.rs
@@ -18,9 +18,7 @@ pub fn describe(input: TokenStream) -> TokenStream {
 pub fn describe_tokio(input: TokenStream) -> TokenStream {
     let section_body = parse_macro_input!(input as Section);
 
-    let output = section_body
-        .with_mode(CatchrMode::Tokio)
-        .to_token_stream();
+    let output = section_body.with_mode(CatchrMode::Tokio).to_token_stream();
 
     output.into()
 }
